b174cd6c6528ebf0b6d74b0b4966c1de5e4db317
CJ Chapman <cj31387@yahoo.com>
2017-12-22 09:29:05 -0800

    Initialize

---
+++ Source/OneShot/OneShotGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneShotGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOneShotGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOneShotGameMode();
+};
+
+
+
